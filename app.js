b2ed72bdc3e39fb1aec1b0559587276073070ac1
igorhristov <igorhristov@sourcemxacademy.mk>
2020-05-12 01:33:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use('/', routes);
 
 // listen for requests :)
-const listener = app.listen(3000 || process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
